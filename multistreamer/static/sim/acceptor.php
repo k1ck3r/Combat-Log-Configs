723c3eda20cc6e4ab51753139b7a202b731d9c0f
Unknown <ymarinov@gmail.com>
2017-11-14 09:04:50 +0200

    simc array scripts

--- multistreamer/static/sim/acceptor.php
+++ multistreamer/static/sim/acceptor.php
@@ -23,7 +23,7 @@ $user_ip = getUserIP();
 
 #echo $user_ip; 
 
-$outputDir='/tmp/sim';
+$outputDir='/opt/multistreamer/static/sim/data';
 if(!file_exists($outputDir))
     @mkdir($outputDir,0775);
 if(file_exists($outputDir) && is_dir($outputDir) && is_writable($outputDir))
@@ -34,7 +34,14 @@ $outputFname=date('Y-m-d_H-i-s-').$user_ip;
 if(isset($_POST['reqData']) && !empty($_POST['reqData'])) {
     $data=$_POST['reqData'];
     file_put_contents($outputDir.$outputFname,$data);
-    shell_exec("/usr/bin/simc  $outputDir$outputFname json2=$outputDir$outputFname.json");
+#   shell_exec("/usr/bin/simc $outputDir$outputFname json2=$outputDir$outputFname.json report_details=1 calculate_scale_factors=1 scale_only=mastery,versatility,haste,crit_rating,agility");
+    shell_exec("/usr/bin/simc $outputDir$outputFname json2=$outputDir$outputFname.json scale_only=mastery,versatility,haste,crit_rating,agility");
+    $json = file_get_contents("https://app.combatlog.eu/sim/data/$outputFname.json");
+    $p = json_decode($json, true);
+    $name = $p['sim']['players'][0]['name'];
+    $race = $p['sim']['players'][0]['race'];
+    $dps = number_format($p['sim']['players'][0]['collected_data']['dpse']['mean'] , 2, '.', ',');
+    echo "name: $name <br>race: $race <br>dps: $dps";
 
 }
 
